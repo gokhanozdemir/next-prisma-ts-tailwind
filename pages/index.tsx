f53e86d43ccbe4955def92dc74d398e96421139f
Gökhan <gn.ozdemir@gmail.com>
2022-03-23 01:02:41 +0300

    Push Note

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-import type { NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
+import { prisma } from '../lib/prisma'
 
 interface FormData {
   title: string,
@@ -10,26 +12,52 @@ interface FormData {
 const Home: NextPage = () => {
   const [form, setForm] = useState<FormData>({ title: "", content: "", id: "" });
 
+  async function create(data: FormData) {
+    try {
+      console.log("fetchStart")
+      fetch('http://localhost:3000/api/create', {
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: "POST"
+      })
+        .then(() => console.log("fetchEnd"))
+        .then(() => setForm({ title: '', content: '', id: '' }))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleSubmit = async (data: FormData) => {
+    try {
+      create(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div>
       <h1 className="text-center font-bold text-2xl mt-4">Notes</h1>
       <form onSubmit={e => {
         e.preventDefault()
-
-      }} className='w-auto min-w-[25%] max-w-min mx-auto space-y-6 flex flex-col items-stretch'>
+        handleSubmit(form)
+      }}
+        className='w-auto min-w-[25%] max-w-min mx-auto space-y-6 flex flex-col items-stretch'>
         <input
           type="text"
           placeholder="Title"
           value={form.title} onChange={e => setForm({ ...form, title: e.target.value })}
-          name=""
-          id=""
+          name="textinp"
+          id="textinp"
           className="border-2 rounded border-gray-200 p-1"
         />
         <textarea
           placeholder="Content"
           value={form.content} onChange={e => setForm({ ...form, content: e.target.value })}
-          name=""
-          id=""
+          name="contentinp"
+          id="contentinp"
           className="border-2 rounded border-gray-200 p-1"
         />
 
