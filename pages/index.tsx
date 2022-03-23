f85cf60218ea3dac194fc385b790933f49d588a3
Gökhan <gn.ozdemir@gmail.com>
2022-03-23 11:47:41 +0300

    Query Notes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
-import { GetServerSideProps, NextPage } from 'next'
-import { useRouter } from 'next/router'
+import type { NextPage } from 'next'
+import { GetServerSideProps } from 'next'
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 import { prisma } from '../lib/prisma'
+
+interface Notes {
+  notes: {
+    title: string,
+    content: string,
+    id: string
+  }
+}
 
 interface FormData {
   title: string,
@@ -9,7 +18,7 @@ interface FormData {
   id: string
 }
 
-const Home: NextPage = () => {
+const Home = ({ notes }: Notes) => {
   const [form, setForm] = useState<FormData>({ title: "", content: "", id: "" });
 
   async function create(data: FormData) {
@@ -63,8 +72,38 @@ const Home: NextPage = () => {
 
         <button type='submit' className='bg-blue-500 text-white rounded p-1'>Add + </button>
       </form>
+      <div className='w-auto min-w-[25%] mt-20 mx-auto space-y-6 flex flex-col items-stretch'>
+        <ul>
+          {notes.map(note => (
+            <li key={note.id} className='border-b border-gray-600 p-2' >
+              <div className='flex justify-between'>
+                <div className='flex-1'>
+                  <h3 className='font-bold'>{note.title}</h3>
+                  <p className='text-sm'>{note.content}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const notes = await prisma.note.findMany({
+    select: {
+      title: true,
+      id: true,
+      content: true
+    }
+  })
+
+  return {
+    props: {
+      notes
+    }
+  }
+}
